5d9107b1c864ebf21b801fc3c1c49c5cfedecaee
Allan Sousa <allan@pop-os.localdomain>
2020-12-03 17:22:52 -0300

    exemplo com st_replace e comentario no codigo

--- index.php
+++ index.php
@@ -3,5 +3,13 @@
 // strlen 
 
 $nome =  "Fernanda";
-echo strlen($nome);
+echo strlen($nome); // realiza a contagem de quantos caractres temos na string
+echo "<br>";
+
+// Str_replace
+$troca_nome = "Bruna Santos";
+$depois_nome = str_replace('Bruna', 'Angelica', $troca_nome);
+/* realiza a troca da primeira palavra pela segunda de maneira agil */
+
+    echo $depois_nome. "<br>";
 
