0726cda855abce1bbbf64a287546aaeb5cfc429e
Allan Sousa <allan@pop-os.localdomain>
2020-12-04 12:01:09 -0300

    inserindo funcoes die e exit ao final do trecho de codigo

---
+++ final_codigo.php
@@ -0,0 +1,16 @@
+<?php
+
+// Die  Função para finalizar um trecho de código
+
+die('Arquivo finalizado');
+$nome =  "Alberto";
+
+echo "{$nome} .<br>";
+
+// Exit Função semelhante a die
+
+exit(12);
+$apelido =  "Franja";
+
+echo $apelido;
+
