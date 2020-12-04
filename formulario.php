5f0b2a7031796178563ad43d78885d7afd585d80
allan sousa <allan@pop-os.localdomain>
2020-12-04 17:03:13 -0300

    formatando formulario

--- formulario.php
+++ formulario.php
@@ -8,17 +8,21 @@
 <body>
     <form action="Senhaemail.php" method="POST">
         <label for="nome">Nome Completo</label>
+        <br> <br>
         <input type="text" name="nome" placeholder="Qual é o seu nome">
         <br><br>
         <label for="Pais">Pais</label>
+        <br> <br>
         <select name="pais" >
-            <option value="Selecione um País"></option>
+            <option value="Selecione um País">Selecione um País</option>
             <option value="brasil">Brasil</option>
             <option value="usa">Estados Unidos</option>
         </select>
         <br><br>
         <label for="mensagem" >Mensagem</label>
-        <textarea name="mensagem" cols="30" rows="10" placeholder="Insira sua mensagem aqui" ></textarea>
+        <br><br>
+        <textarea name="mensagem" cols="20" rows="05" placeholder="Insira sua mensagem aqui" ></textarea>
+        <br><br>
         <input type="submit" value="Enviar">        
     </form>
 </body>
