b59730714a71a39e47cb89ba80469d014c1707d5
Allan Sousa <allan@pop-os.localdomain>
2020-12-04 13:39:42 -0300

    estudos sobre a variavel global session nos documentos sessao.php e cor.php

---
+++ cor.php
@@ -0,0 +1,4 @@
+<?php
+ session_start();
+
+echo "Minha cor favorita é ". $_SESSION['cor_favorita'] ?? 'não informada';
---
+++ sessao.php
@@ -0,0 +1,7 @@
+<?php
+
+session_start();
+
+$_SESSION['cor_favorita'] = "azul";
+
+echo "cor favorita setada";
