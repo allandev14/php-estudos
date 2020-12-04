35d5152417579bbf2030787d1d73d0e8506de308
Allan Sousa <allan@pop-os.localdomain>
2020-12-04 12:31:18 -0300

    inserindo expressoes numericas com  a funcao number_format

---
+++ number_format.php
@@ -0,0 +1,10 @@
+<?php
+
+// number_format  Função para formatação de númerica
+// Muito usado para formatação de valores finaceiros 
+
+echo number_format("10000"). "<br>";
+echo number_format("10000", 2). "<br>";
+echo number_format("1000.45", 2). "<br>";
+echo number_format("10000", 2, ",", "."). "<br>";
+echo number_format("10000.45", 4, ",", "."). "<br>";
